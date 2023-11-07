43a45c17c14b9a996c6dd9207703253a46a0d404
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-11-07 09:25:08 -0300

    hard codded api

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-const Logger = require('./logger')
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-const logger = new Logger();
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.readFile(
+      path.join(__dirname, "public", "index.html"),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
+  if (req.url === "/api/users") {
+    const users = [
+      {
+        name: "Bob Smith",
+        age: 40,
+      },
+      { name: "John Dow", age: 35 },
+    ];
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(users));
+  }
+});
 
-logger.on('message', data => console.log('Called Listener' ,data))
+const PORT = process.env.PORT || 5000;
 
-logger.log('hello World')
-logger.log('hi!')
-logger.log('hello!')
+server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
